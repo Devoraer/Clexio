de7b764520f2fdaecbe63e7244fd8c3373f9258d
leahgubman <leahgubman@gmail.com>
2025-07-14 20:11:00 +0300

    login x

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,8 @@
-import LoginPage from "./pages/LoginPage"; 
-import HomePage from "./components/HomePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css';
+
+// ✅ עמודים
+ import HomePage from "./components/HomePage";
 import MachinesDashboard from "./pages/MachinesDashboard";
 import MaterialsDashboard from "./pages/MaterialsDashboard";
 import FrontDashboard from "./pages/FrontDashboard";
@@ -7,21 +10,27 @@ import SamplesDashboard from "./pages/SamplesDashboard";
 import AddStabilityForm from "./pages/AddStabilityForm";
 import AddSample from "./components/AddSample";
 import ViewStability from "./pages/ViewStability";
+import CalendarPage from "./pages/CalendarPage";
 
+// ✅ Layout
+import MainLayout from "./MainLayout";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/dashboard" element={<FrontDashboard />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/materials" element={<MaterialsDashboard />} />
-        <Route path="/machines" element={<MachinesDashboard />} />
-        <Route path="/samples" element={<SamplesDashboard />} />
-        <Route path="/add-stability" element={<AddStabilityForm />} />
-        <Route path="/add-sample" element={<AddSample />} />
-        <Route path="/view-stability/:id" element={<ViewStability />} />
+
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/dashboard" element={<FrontDashboard />} />
+          <Route path="/materials" element={<MaterialsDashboard />} />
+          <Route path="/machines" element={<MachinesDashboard />} />
+          <Route path="/samples" element={<SamplesDashboard />} />
+          <Route path="/add-stability" element={<AddStabilityForm />} />
+          <Route path="/add-sample" element={<AddSample />} />
+          <Route path="/view-stability/:id" element={<ViewStability />} />
+          <Route path="/calendar" element={<CalendarPage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
